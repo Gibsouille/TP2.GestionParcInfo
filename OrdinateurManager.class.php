7e57b9379b2486a0de1ca61f9c028fd16078ff70
unknown <benjamin.lamard@ecole-isitech.fr>
2018-01-16 17:01:19 +0100

    Refactor dans le manager

--- OrdinateurManager.class.php
+++ OrdinateurManager.class.php
@@ -9,31 +9,20 @@ class OrdinateurManager {
     }
     
     public function listerLesOrdinateurs() {
-        $listeOrdinateurs = array();
-        
-        try {
-            $selectOrdinateurs = "select * from ordinateurs order by ip asc";
-            $reponse = $this->connexionDB->query($selectOrdinateurs);
-
-            while (($ligneReponse = $reponse->fetch())) {
-                $ordinateur = new Ordinateur($ligneReponse['id'], $ligneReponse['ip'], $ligneReponse['mac'], $ligneReponse['nom'], $ligneReponse['salle']);
-                array_push($listeOrdinateurs, $ordinateur);
-            }
-
-            $reponse->closeCursor();
-        } catch (PDOException $ex) {
-            echo '<pre>Erreur : ' . $ex.getMessage() . '</pre>';
-        }
-        
-        return $listeOrdinateurs;
+        $requete = "select * from ordinateurs order by ip asc";
+        return $this->listerOrdinateursDepuisCetteRequete($requete);
     }
     
     public function listerLesOrdinateursAvecCeDebutIp($debutIp) {
+        $requete = "select * from ordinateurs where ip like '" . $debutIp . "%' order by ip asc";
+        return $this->listerOrdinateursDepuisCetteRequete($requete);
+    }
+    
+    private function listerOrdinateursDepuisCetteRequete($requete) {
         $listeOrdinateurs = array();
         
         try {
-            $selectOrdinateurs = "select * from ordinateurs where ip like '" . $debutIp . "%' order by ip asc";
-            $reponse = $this->connexionDB->query($selectOrdinateurs);
+            $reponse = $this->connexionDB->query($requete);
 
             while (($ligneReponse = $reponse->fetch())) {
                 $ordinateur = new Ordinateur($ligneReponse['id'], $ligneReponse['ip'], $ligneReponse['mac'], $ligneReponse['nom'], $ligneReponse['salle']);
